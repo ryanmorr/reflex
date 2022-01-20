c4f69eebdb2a4ebf6d35b87a765e19b54ab54201
Ryan Morr <rm.morr@gmail.com>
2022-01-19 19:39:35 -0500

    Change variable name for consistency

--- src/effect.js
+++ src/effect.js
@@ -3,13 +3,13 @@ import { uuid } from './util';
 
 export function effect(...deps) {
     let initialized = false;
-    const id = uuid();
+    const key = uuid();
     const callback = deps.pop();
     const values = [];
     deps.forEach((dep, i) => dep.subscribe((value) => {
         values[i] = value;
         if (initialized) {
-            addEffect(id, () => callback(...values));
+            addEffect(key, () => callback(...values));
         }
     }));
     initialized = true;
