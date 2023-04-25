e43c72fd20b2555988a829c781392b1cdd5e32fe
Ryan Morr <rm.morr@gmail.com>
2023-04-25 16:56:52 -0400

    Remove when function

--- src/reflex.js
+++ src/reflex.js
@@ -3,6 +3,5 @@ export { html } from './html';
 export { effect } from './effect';
 export { bind } from './bind';
 export { each } from './each';
-export { when } from './when';
 export { cleanup, dispose } from './disposal';
 export { tick } from './scheduler';
